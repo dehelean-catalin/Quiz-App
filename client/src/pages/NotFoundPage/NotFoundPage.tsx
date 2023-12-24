9fc5873880f346a42975801130f4028ec944c048
dehelean-catalin <drcatalin00@gmail.com>
2023-12-24 13:50:43 +0200

    Not found page design

--- client/src/pages/NotFoundPage/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,24 @@
+import { useNavigate } from "react-router";
+import styles from "./NotFoundPage.module.css";
+
 export function NotFoundPage() {
-	return <div>NotFoundPage</div>;
+	const navigate = useNavigate();
+
+	function handleNavigateBack() {
+		navigate(-1);
+	}
+
+	return (
+		<div className={styles.container}>
+			<h1>
+				Oops, <span>nothing</span> here ...
+			</h1>
+			<p>
+				We can't find the page you are looking for. Try going back to previous
+				page or Contact us for more information
+			</p>
+
+			<button onClick={handleNavigateBack}>Go back</button>
+		</div>
+	);
 }
